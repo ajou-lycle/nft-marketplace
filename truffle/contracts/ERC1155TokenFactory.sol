562a0f83403f538af7043e52408a4fde5179e335
Long9725 <develop9725@gmail.com>
2022-10-14 20:45:50 +0900

    refactoring: move network from ropsten to goerli

--- truffle/contracts/ERC1155TokenFactory.sol
+++ truffle/contracts/ERC1155TokenFactory.sol
@@ -2,31 +2,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "./ERC1155Token.sol";
+import "./ERC1155TokenEnumerable.sol";
 
-contract ERC1155TokenFactory {
+contract ERC1155TokenFactory is ERC1155TokenEnumerable {
     event CreateNewERC1155Token(string name, address contractAddress);
 
-    mapping(string => address) private _contractAddresses;
     ERC1155Token[] public ERC1155TokenArray;
 
     constructor() {}
 
-    function createNewERC1155Token(string memory name, string memory symbol, string memory baseURI) public {
-        require(_contractAddresses[name] == address(0x0), "There is already existed contract with name");
-
+    function createNewERC1155Token(string memory name, string memory symbol, string memory baseURI) public onlyEmptyName(name) {
         ERC1155Token erc1155Token = new ERC1155Token(name, symbol, baseURI, msg.sender);
         ERC1155TokenArray.push(erc1155Token);
 
-        address contractAddress = address(erc1155Token);
-        _contractAddresses[name] = contractAddress;
-        emit CreateNewERC1155Token(name, contractAddress);
+        setContractAddressByName(name, address(erc1155Token));
+
+        emit CreateNewERC1155Token(name, address(erc1155Token));
     }
 
     function totalDeployedContract() public view returns(uint256) {
         return ERC1155TokenArray.length;
-    }
-
-    function getContractAddressByName(string memory name) public view returns(address) {
-        return _contractAddresses[name];
     }
 }
--- truffle/migrations/1_deploy_ERC1155TokenFactory.js
+++ truffle/migrations/1_deploy_ERC1155TokenFactory.js
@@ -1,6 +1,6 @@
 const ERC1155TokenFactory = artifacts.require("ERC1155TokenFactory");
 require("dotenv").config({path: "../.env"});
 
-module.exports = async function (deployer) {
+module.exports = function (deployer) {
     deployer.deploy(ERC1155TokenFactory);
 };
