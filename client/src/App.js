8ad4d12725dad6df8365427f734aecfd64ffd093
Long9725 <develop9725@gmail.com>
2022-11-05 14:50:16 +0900

    fix: webpack 5 with web3

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,12 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import RootRoute from "./routes";
 import ScrollTop from "./ScrollTop";
-import EthProvider from './contexts/EthContext/EthProvider';
 
 function App() {
   return (
     <RecoilRoot>
       <ScrollTop />
-      <EthProvider>
         <RootRoute />
-      </EthProvider>
     </RecoilRoot>
   );
 }
